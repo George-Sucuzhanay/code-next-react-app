81fbe676d273f49a00515544b2b6313a766b7265
George-Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-10-05 17:47:12 -0400

    2 files modified

--- src/index.js
+++ src/index.js
@@ -1,119 +1,18 @@
-/* let allows of it to be reassigned in contrast to const 
+// get the element that has [id="count"]
+const countSpan = document.getElementById('count');
 
-let foo = 'foo';
-foo = 'bar';
-
-booleans 
-const isLoaded = undefined;
-console.log(!!isLoaded);
+// updates the inner text immediately
+countSpan.innerText = 42;
 
 
-Number 
-const number = 3;
-console.log(number);
+// declare a mutable variable called `count` and initialize it to 0
+let count = 0;
 
-const sym = Symbol.for('asdfa');
-console.log(sym);
+// get the element that has [id="increment"]
+const incrementButton = document.getElementById('increment');
 
-const largeNumber = BigInt(123232323232);
-
-const foo =  'foo';
-
-console.log(typeof foo);
-
-
-Objects - container with keys/ keys that are strings 
-
-const o = {
-  whats: {
-    my: {
-      secret: 'asfasdf'
-    }
-  }
-
-};
-console.log(o);
-
-const o = {
-  whats: {
-    my: {
-      secret: 'asfasdf'
-    }
-  }
-
-};
-console.log(o);
-*/
-
-const o = {
-  hello: 'world'
-};
-const n = {...o};
-
-o.hello = 'goodbye';
-
-console.log(n);
-console.log(o);
-
-/* Arrays */
-
-const arr = [1, 'two', {three: 'three'}];
-const arrCopy = [...arr];
-arr[0] = "asdfasdfasdfasdf"
-console.log(arr);
-
-console.log(arr[0]);
-
-
-/* Functions are the building blocks of programming. Functions are considered like objects similar to first class ppl. :-) */
-
-function add (a,b) {
-
+// when the button is clicked, invoke a function that increments count
+incrementButton.onclick = () => {
+  count++
+  // you'll need to do something else to update the count display
 }
-
-const add = (a,b) => {
-  console.log(this);
-  const sum = a + b;
-  return sum;
-}
-add();
-
-const identity  = (str) => str;
-const scream = (fn,str) => {
-  console.log(fn(sttr)).toUpperCase();
-
-}
-scream(identity), 'asdf';
-
-const getLie = () => {
-  return 'this is boring no cap';
-
-const getTruth = () => {
-  return 'this is cool no cap';
-
-}
-};
-const scream = (getMessage) => {
-  console.log(getMessage().toUpperCase());
-}
-
-scream(getLie);
-scream(getTruth);
-
-
-
-const getLie = () => 'asdfasdf';
-getLie.foo = 'hello';
-
-console.log(getLie.foo);
-/* functions csn return function */
-
-const deferredSum = (a,b) => {
-  const i = a+b;
-  return () => i + c;
-}
-const sum = derferredSum(1,2,3);
-console.log(sum());
-
-
-const 
