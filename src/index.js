4e0caae099fcb45b6a460fc7e43cf87e028b3145
George-Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-10-05 17:00:20 -0400

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-const jsApp = document.getElementById('js-app');
-jsApp.innerHTML = '<h1>Hello from the DOM!</h1>';
-console.log('Hello from the console!');
+/* let allows of it to be reassigned in contrast to const 
+
+let foo = 'foo';
+foo = 'bar';
+
+booleans 
+const isLoaded = undefined;
+console.log(!!isLoaded);
+
+
+Number 
+const number = 3;
+console.log(number);
+
+const sym = Symbol.for('asdfa');
+console.log(sym);
+
+const largeNumber = BigInt(123232323232);
+
+const foo =  'foo';
+
+console.log(typeof foo);
+
+
+Objects - container with keys/ keys that are strings 
+
+const o = {
+  whats: {
+    my: {
+      secret: 'asfasdf'
+    }
+  }
+
+};
+console.log(o);
+
+const o = {
+  whats: {
+    my: {
+      secret: 'asfasdf'
+    }
+  }
+
+};
+console.log(o);
+*/
+
+const o = {
+  hello: 'world'
+};
+const n = {...o};
+
+o.hello = 'goodbye';
+
+console.log(n);
+console.log(o);
