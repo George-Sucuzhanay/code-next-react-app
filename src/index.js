efbe18aa7d0a187ed7d08774880103624a90a4cd
George-Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-10-05 17:53:35 -0400

    1 file modified

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
+// declare a mutable variable called `count` and initialize it to 0
+let count = 0;
+
 // get the element that has [id="count"]
 const countSpan = document.getElementById('count');
+
+
+// get the element that has [id="increment"]
+const incrementButton = document.getElementById('increment');
+
+// get the element that has [id="decrement"]
+const decrementButton = document.getElementById('decrement');
+
+
+const updateCount = () => {
+  countSpan.innerHTML = count;
+}
+// when the button is clicked, invoke a function that increments count
+incrementButton.onclick = () => {
+  count++
+  // you'll need to do something else to update the count display
+  updateCount();
+}
+
+
+decrementButton.onclick = () => {
+  count--
+  updateCount();
+
+}
+
 
 // updates the inner text immediately
 countSpan.innerText = 42;
 
 
-// declare a mutable variable called `count` and initialize it to 0
-let count = 0;
-
-// get the element that has [id="increment"]
-const incrementButton = document.getElementById('increment');
-
-// when the button is clicked, invoke a function that increments count
-incrementButton.onclick = () => {
-  count++
-  // you'll need to do something else to update the count display
-}
