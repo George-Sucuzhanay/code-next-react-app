bac8358ecc8525fdf9c807cc2afd614915d309a8
George-Sucuzhanay <georgesucuzhanay100@gmail.com>
2020-10-06 22:50:08 -0400

    1 file modified, 1 added

---
+++ src/Counter.js
@@ -0,0 +1,11 @@
+import React, { useState} from 'react';
+
+export const Counter = () => {
+  const [count],setCount = useState[0];
+  return {
+    <div>
+    count {count}
+    </div>
+      <button onClick
+  }
+}
